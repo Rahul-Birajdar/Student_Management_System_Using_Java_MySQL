9e3c60026ac343d69cb5f65dd1f01a49d8996784
Rahul-Birajdar <113769852+Rahul-Birajdar@users.noreply.github.com>
2025-03-08 00:20:07 +0530

    file added

---
+++ GradientPanel.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+class GradientPanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        int width = getWidth();
+        int height = getHeight();
+        Color color1 = new Color(255, 218, 185); // Peach
+        Color color2 = new Color(233, 150, 122); // Dark Salmon
+        GradientPaint gp = new GradientPaint(0, 0, color1, 0, height, color2);
+        g2d.setPaint(gp);
+        g2d.fillRect(0, 0, width, height);
+    }
+}
